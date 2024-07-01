3247b648a0f3bf6e06e9be42ef614a65cb121f06
MADHUSREE BERA <73298219+MadhusreeBera@users.noreply.github.com>
2024-07-01 23:42:48 +0530

    Add files via upload

---
+++ Activity 1/SimpleRA/src/executors/source.cpp
@@ -0,0 +1,38 @@
+#include "global.h"
+/**
+ * @brief 
+ * SYNTAX: SOURCE filename
+ */
+bool syntacticParseSOURCE()
+{
+    logger.log("syntacticParseSOURCE");
+    if (tokenizedQuery.size() != 2)
+    {
+        cout << "SYNTAX ERROR" << endl;
+        return false;
+    }
+    parsedQuery.queryType = SOURCE;
+    parsedQuery.sourceFileName = tokenizedQuery[1];
+    return true;
+}
+
+bool semanticParseSOURCE()
+{
+    logger.log("semanticParseSOURCE");
+    if (!isQueryFile(parsedQuery.sourceFileName))
+    {
+        cout << "SEMANTIC ERROR: File doesn't exist" << endl;
+        return false;
+    }
+    return true;
+}
+
+void executeSOURCE()
+{
+    logger.log("executeSOURCE");
+    cout << "Executing SOURCE" << endl;
+    syntacticParseSOURCE();
+    semanticParseSOURCE();
+    cout << parsedQuery.sourceFileName << endl;
+    return;
+}
